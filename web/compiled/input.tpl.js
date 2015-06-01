77e7646a3a20215128aae057f3b38e65766342bd
Zoltan Biro <Zoltan_Biro@epam.com>
2015-06-01 11:49:56 +0200

    closeable popup

--- web/compiled/input.tpl.js
+++ web/compiled/input.tpl.js
@@ -2,15 +2,15 @@ this["Handlebars"] = this["Handlebars"] || {};
 this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
 
 this["Handlebars"]["templates"]["web/templates/input.handlebars"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
-    return "        <span class=\"error\"><%_inputerror%></span>\n";
+    return "        <span class=\"error\"><%_inputerror%></span>\r\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<div id=\"content\">\n<button class=\"clickable buttonsmall\" onclick=\"setLanguage('en')\" value=\"en\" >EN</button>\n<button class=\"clickable buttonsmall\" onclick=\"setLanguage('ru')\" value=\"ru\" >RU</button>\n<br/>\n    <div id=\"maininput\">\n"
+  return "<div id=\"content\">\r\n<button class=\"clickable buttonsmall\" onclick=\"setLanguage('en')\" value=\"en\" >EN</button>\r\n<button class=\"clickable buttonsmall\" onclick=\"setLanguage('ru')\" value=\"ru\" >RU</button>\r\n<br/>\r\n    <div id=\"maininput\">\r\n"
     + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.inputerror : depth0),{"name":"if","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "        <p><label><%_name%></label><input type=\"text\" id=\"summonerName\" value=\""
     + alias3(((helper = (helper = helpers.prename || (depth0 != null ? depth0.prename : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"prename","hash":{},"data":data}) : helper)))
-    + "\" name=\"summonerName\" /></p>\n        <p><label><%_region%> </label><input type=\"text\" id=\"region\" value=\""
+    + "\" name=\"summonerName\" /></p>\r\n        <p><label><%_region%> </label><input type=\"text\" id=\"region\" value=\""
     + alias3(((helper = (helper = helpers.preregion || (depth0 != null ? depth0.preregion : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"preregion","hash":{},"data":data}) : helper)))
-    + "\" name=\"region\" /></p>\n        <p><input type=\"checkbox\" id=\"rememberme\" ><label for=\"rememberme\"><%_remember%></label></p>\n        <button class=\"clickable button\" onclick=\"eventAgg.trigger('input:getTeam')\" value=\"Match me with retards!\" ><%_inputbutton%></button>\n    </div>\n    <div id=\"endorse\">\n        <span>This software  isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</span>\n    </div>\n</div>\n";
+    + "\" name=\"region\" /></p>\r\n        <p><input type=\"checkbox\" id=\"rememberme\" ><label for=\"rememberme\"><%_remember%></label></p>\r\n        <button class=\"clickable button\" onclick=\"eventAgg.trigger('input:getTeam')\" value=\"Match me with retards!\" ><%_inputbutton%></button>\r\n    </div>\r\n    <div id=\"endorse\">\r\n        <span>This software  isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</span>\r\n    </div>\r\n</div>\r\n";
 },"useData":true});
--- web/compiled/popup.tpl.js
+++ web/compiled/popup.tpl.js
@@ -4,17 +4,17 @@ this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
 this["Handlebars"]["templates"]["web/templates/popup.handlebars"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var alias1=this.lambda, alias2=this.escapeExpression;
 
-  return "                <tr>\r\n                    <td>"
+  return "                    <tr>\r\n                        <td>"
     + alias2(alias1((depth0 != null ? depth0.count : depth0), depth0))
-    + "</td>\r\n                    <td>"
+    + "</td>\r\n                        <td>"
     + alias2(alias1((depth0 != null ? depth0.description : depth0), depth0))
-    + "</td>\r\n                </tr>\r\n";
+    + "</td>\r\n                    </tr>\r\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div id=\"runesandmasteries\">\r\n    <h2><%_masteries%>: </h2><h3>"
+  return "<div id=\"runesandmasteries\">\r\n    <div>\r\n        <h2 class=\"left\"><%_masteries%>: </h2> <h3 class=\"left\">"
     + this.escapeExpression(this.lambda((depth0 != null ? depth0.masteries : depth0), depth0))
-    + "</h3>\r\n    <h2><%_runes%>: </h2>\r\n         <table id=\"runes\">\r\n            <thead>\r\n                <th><%_count%></th>\r\n                <th><%_description%></th>\r\n            <thead/>\r\n"
+    + "</h3> <spam id=\"close\" onclick=\"eventAgg.trigger('popup:hide')\">X</spam>\r\n    </div>\r\n    <div id=\"runetable\">\r\n        <h2><%_runes%>: </h2>\r\n             <table id=\"runes\">\r\n                <thead>\r\n                    <th><%_count%></th>\r\n                    <th><%_description%></th>\r\n                <thead/>\r\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.runes : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "        </table>\r\n</div>";
+    + "            </table>\r\n    </div>\r\n</div>";
 },"useData":true});
